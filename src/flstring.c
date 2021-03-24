e76704da9ebd05d2821576b91df3d9d6aa44a9b8
Albrecht Schlosser <albrechts.fltk@online.de>
2021-03-24 20:31:59 +0100

    Fix and document fl_strlcat() and fl_strlcpy()
    Catch potential error if the destination buffer is not NUL-terminated.
    
    Add doxygen docs.
    
    Todo: the docs should only be available for developers.

--- src/flstring.c
+++ src/flstring.c
@@ -1,7 +1,7 @@
 /*
  * BSD string functions for the Fast Light Tool Kit (FLTK).
  *
- * Copyright 1998-2010 by Bill Spitzak and others.
+ * Copyright 1998-2021 by Bill Spitzak and others.
  *
  * This library is free software. Distribution and use rights are outlined in
  * the file "COPYING" which should have been included with this file.  If this
@@ -15,28 +15,63 @@
  */
 
 #include "flstring.h"
+#include <string.h>
 
+/** \file src/flstring.c
 
-/*
- * 'fl_strlcat()' - Safely concatenate two strings.
- */
+  String handling functions.
+*/
+
+/** Safely concatenate two strings.
+
+  This function is a replacement for the BSD function strlcat() (which is not
+  available on all supported platforms) with some minor differences. It is
+  compatible with strlcat() except for buffer overflow handling (see below).
+
+  The source and destination strings \b must be NUL-terminated strings.
+  The destination buffer may be empty, i.e. only contain a NUL-byte.
+
+  The function appends the source string \p src to the destination string \p dst
+  if there is room. The string is truncated if there's not enough room for the
+  entire string.
+
+  Contrary to the BSD function strlcat() there is no overflow indication. If the
+  concatenated string would overflow the buffer, the result string (buffer) will
+  contain only \p size - 1 bytes and the return value will be at most \p size - 1.
+
+  \note In the special case that the input string is not NUL-terminated (which
+    would be a programming error), fl_strlcat() returns \p size and the buffer
+    is not modified (no NUL-termination). The return value \p size indicates
+    that the string in the buffer was and is not NUL-terminated. In this error
+    case this function will access memory beyond the limits of \p dst + (plus)
+    \p size because strlen() is used to determine the \p dst length.
+
+  This function never overflows the buffer length given by \p size.
+
+  \param[inout] dst     destination string buffer (modified)
+  \param[in]    src     source string
+  \param[in]    size    destination buffer size
+
+  \returns    Length of concatenated string
+*/
 
 size_t                          /* O - Length of string */
-fl_strlcat(char       *dst,     /* O - Destination string */
+fl_strlcat(char       *dst,     /* O - Destination string buffer */
            const char *src,     /* I - Source string */
            size_t     size) {   /* I - Size of destination string buffer */
   size_t        srclen;         /* Length of source string */
   size_t        dstlen;         /* Length of destination string */
 
-
  /*
   * Figure out how much room is left...
+  * Caution: take care of arithmetic with unsigned variables!
   */
 
   dstlen = strlen(dst);
-  size   -= dstlen + 1;
+  if (dstlen + 1 >= size)
+    return (dstlen);            /* No room, return immediately... */
 
-  if (!size) return (dstlen);   /* No room, return immediately... */
+  size -= dstlen + 1;
 
  /*
   * Figure out how much room is needed...
@@ -57,23 +92,48 @@ fl_strlcat(char       *dst,     /* O - Destination string */
 }
 
 
-/*
- * 'fl_strlcpy()' - Safely copy two strings.
- */
+/** Safely copy a string.
+
+  This function is a replacement for the BSD function strlcpy() (which is not
+  available on all supported platforms) with some minor differences. It is
+  compatible with strlcpy().
+
+  The source string \p src \b must be a NUL-terminated string.
+
+  The function copies the source string \p src to the destination buffer \p dst.
+  The string is truncated if there's not enough room for the entire string.
+
+  Contrary to the BSD function strlcpy() there is no overflow indication. If the
+  copied string would overflow the buffer, the result string (buffer) will
+  contain only \p size - 1 bytes and the return value will be at most \p size - 1.
+
+  This function never overflows the buffer length given by \p size and the
+  string in the destination buffer is always NUL-terminated.
+
+  If the destination buffer \p size is zero, the return value is zero and
+  nothing is copied.
+
+  \param[inout] dst     destination string buffer (modified)
+  \param[in]    src     source string
+  \param[in]    size    destination buffer size
+
+  \returns    Length of copied string
+*/
 
 size_t                          /* O - Length of string */
 fl_strlcpy(char       *dst,     /* O - Destination string */
            const char *src,     /* I - Source string */
            size_t      size) {  /* I - Size of destination string buffer */
-  size_t        srclen;         /* Length of source string */
 
+  size_t srclen;                /* Length of source string */
 
  /*
   * Figure out how much room is needed...
   */
 
-  size --;
-
+  if (size == 0)                /* no room in buffer */
+    return 0;
+  size--;
   srclen = strlen(src);
 
  /*
@@ -81,18 +141,20 @@ fl_strlcpy(char       *dst,     /* O - Destination string */
   */
 
   if (srclen > size) srclen = size;
-
-  memcpy(dst, src, srclen);
+  if (srclen > 0) {
+    memcpy(dst, src, srclen);
+  }
   dst[srclen] = '\0';
-
   return (srclen);
 }
+
 
 #define C_RANGE(c,l,r) ( (c) >= (l) && (c) <= (r) )
 
 /**
- * locale independent ascii oriented case cmp
- * returns 0 if string successfully compare, -1 if s<t, +1 if s>t
+ * Locale independent ASCII oriented case compare function.
+ *
+ * returns 0 if strings successfully compare, -1 if s < t, +1 if s > t.
  */
 int fl_ascii_strcasecmp(const char *s, const char *t) {
   if (!s || !t) return (s==t ? 0 : (!s ? -1 : +1));
