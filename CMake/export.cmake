66e51494fa9fcf6d17ae9f617c88cc6ad439bbbd
Albrecht Schlosser <albrechts.fltk@online.de>
2024-02-07 16:00:40 +0100

    Fix build w/o fluid in some special cases

--- CMake/export.cmake
+++ CMake/export.cmake
@@ -1,5 +1,6 @@
 #
 # Export CMake file to build the FLTK project using CMake (www.cmake.org)
+#
 # Originally written by Michael Surette
 #
 # Copyright 1998-2024 by Bill Spitzak and others.
@@ -22,30 +23,49 @@
 # Set the fluid executable path used to create .cxx/.h from .fl files
 
 if(FLTK_BUILD_FLUID AND NOT CMAKE_CROSSCOMPILING)
-  # use the fluid executable we build
+  # Use the fluid executable we build using its namespaced target name
   if(WIN32)
-    set(FLTK_FLUID_EXECUTABLE fluid-cmd)
+    set(FLTK_FLUID_EXECUTABLE fltk::fluid-cmd)
     set(FLUID_EXPORT fluid fluid-cmd)      # export fluid and fluid-cmd
   else()
-    set(FLTK_FLUID_EXECUTABLE fluid)
+    set(FLTK_FLUID_EXECUTABLE fltk::fluid)
     set(FLUID_EXPORT fluid)                # export fluid
   endif()
 else()
-  # find a fluid executable on the host system
-  find_file(FLUID_PATH
+  # We don't build fluid /or/ we are cross-compiling (or both):
+  # we need to find a fluid executable on the build host.
+  # The search is restricted to the user's PATH (environment).
+  find_program(FLTK_FLUID_HOST
     NAMES fluid fluid.exe
     PATHS ENV PATH
+    NO_DEFAULT_PATH
     NO_CMAKE_FIND_ROOT_PATH
   )
-  set(FLTK_FLUID_EXECUTABLE ${FLUID_PATH})
+  if(NOT FLTK_FLUID_HOST)
+    message(STATUS "Warning: fluid not found on the build system!")
+  endif()
+  # Note: this *may* assign "FLTK_FLUID_HOST-NOTFOUND"
+  set(FLTK_FLUID_EXECUTABLE "${FLTK_FLUID_HOST}")
   set(FLUID_EXPORT "")                     # don't export fluid
 endif(FLTK_BUILD_FLUID AND NOT CMAKE_CROSSCOMPILING)
 
+if(0) # Debug
+  message(STATUS "##############################################################")
+  message(STATUS "[export.cmake] INFO: Did we find fluid?")
+  fl_debug_var(FLTK_FLUID_HOST)
+  fl_debug_var(FLTK_FLUID_EXECUTABLE)
+  fl_debug_var(FLTK_BUILD_FLUID)
+  fl_debug_var(CMAKE_CROSSCOMPILING)
+  message(STATUS "##############################################################")
+endif()
+
 # generate FLTK-Targets.cmake for build directory use
-export (TARGETS
-        ${FLUID_EXPORT} ${FLTK_LIBRARIES}
-        NAMESPACE fltk::
-        FILE ${CMAKE_CURRENT_BINARY_DIR}/FLTK-Targets.cmake)
+export(TARGETS
+          ${FLUID_EXPORT} ${FLTK_LIBRARIES}
+       FILE
+          ${CMAKE_CURRENT_BINARY_DIR}/FLTK-Targets.cmake
+       NAMESPACE
+          fltk::)
 
 # generate FLTK-Functions.cmake for build directory use
 configure_file(
@@ -56,9 +76,10 @@ configure_file(
 
 # generate FLTKConfig.cmake for build directory use
 set(INCLUDE_DIRS "${FLTK_INCLUDE_DIRS}")
-if(FLTK_HAVE_CAIRO)
+if(FLTK_HAVE_CAIRO OR FLTK_USE_CAIRO)
   list(APPEND INCLUDE_DIRS ${PKG_CAIRO_INCLUDE_DIRS})
 endif()
+list(REMOVE_DUPLICATES INCLUDE_DIRS)
 set(CONFIG_PATH ${CMAKE_CURRENT_BINARY_DIR})
 
 configure_file(
@@ -83,8 +104,8 @@ configure_file(
   @ONLY
 )
 
-# Set execute permissions on fltk-config in build dir
-# Note: file(CHMOD) available since CMake 3.19,
+# Set execute permissions on fltk-config in the build directory.
+# Note: file(CHMOD) is available since CMake 3.19,
 # use fallback before CMake 3.19
 
 if(CMAKE_VERSION VERSION_LESS 3.19)
